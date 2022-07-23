585e843d5bef550411b4f9e590af28755ecf96ec
KimTou <752160655@qq.com>
2022-07-23 20:27:53 +0800

    配置feign降级方法

---
+++ connector-2/src/main/java/cn/tojintao/feign/fallback/ChatServiceFallback.java
@@ -0,0 +1,24 @@
+package cn.tojintao.feign.fallback;
+
+import cn.tojintao.feign.ChatService;
+import cn.tojintao.model.dto.ResultInfo;
+import cn.tojintao.model.entity.GroupMessage;
+import cn.tojintao.model.entity.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author cjt
+ * @date 2022/6/20 17:03
+ */
+@Component
+public class ChatServiceFallback implements ChatService {
+    @Override
+    public ResultInfo<Message> saveMessage(Message message) {
+        return new ResultInfo<>(500, "远程调用失败");
+    }
+
+    @Override
+    public ResultInfo<GroupMessage> saveGroupMessage(GroupMessage groupMessage) {
+        return new ResultInfo<>(500, "远程调用失败");
+    }
+}
